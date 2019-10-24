90a3c834ce4964436b9e8669ee7b64fd154d12eb
marizoldi <marizadima@yahoo.com>
2019-10-24 14:37:10 +0100

    my first attempt

---
+++ main.py
@@ -0,0 +1,9 @@
+price = 20
+tax = 0.409876543
+snake_string = "'THERE's a snake in my boot'"
+_del = 4
+new_variable = "another hello"
+
+print(new_variable)
+
+print("The tax is {},{:.3f},{} ".format(price, tax, snake_string))
